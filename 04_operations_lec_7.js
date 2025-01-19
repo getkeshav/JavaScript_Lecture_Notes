6a1e3585229a865ef35b5d24ed1ea12607beecb5
Pranaw-NIT-JSR <pramadhu.kumar@gmail.com>
2025-01-19 19:25:35 +0530

    lec-7 done

---
+++ 04_operations_lec_7.js
@@ -0,0 +1,40 @@
+//Basic operaions
+
+console.table([3+2, 3-2, 3*2, 3/2, 3**2, 3%2])
+
+/*
+
+1. ** is used for a^b
+2. % is used for modulo (remainder when a divided by b)
+
+*/
+
+
+//Concatenation of a string
+
+let str1 = "Hello"
+let str2 = " Pranaw"
+
+let finalString = str1 + str2
+
+console.log(finalString)
+
+//Complex cases of number and string
+
+//All outputs are of string
+
+console.log("1" + "2") // 12
+console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+
+console.log("1" + 3 + 2) //132 
+console.log(1 + 3 + "2") // 42 (since left to right execution)
+
+//Assigning same value to multiple variables
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2; //This works because assignment operator moves from right to left
+
+//Prefix and Postfix operators- Read about it (researcha bout it in MDN docs)
+
