643f9ff9faef9fba74c7b9df4cd052fb72f0ace9
Pranaw-NIT-JSR <pramadhu.kumar@gmail.com>
2025-01-19 20:12:55 +0530

    lec-9 done

---
+++ Basics/06_dataTypes_lec_9.js
@@ -0,0 +1,23 @@
+//Check notes in notebook- Primitive and Non-Primitive (reference type)
+
+//Symbol discussion (Primitve data type)
+
+const id1 = Symbol ('123')
+const id2 = Symbol ('123')
+
+console.log(id1 === id2) //false. becuase even though values passed in both are same, but teh symbol becomes different, since both symbols are unique
+
+let example = function() {
+    console.log("Hi");
+};
+
+//Typeof different types of data
+
+let a = typeof example //function
+let b = typeof null //object
+let c = typeof undefined //undefined
+let d = typeof id1 //symbol
+
+console.table([a, b, c, d])
+
+ 
