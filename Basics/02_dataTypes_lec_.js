8131587930f91cead849e0758b1290cc4ab5e7eb
Pranaw-NIT-JSR <pramadhu.kumar@gmail.com>
2025-01-19 18:10:35 +0530

    lec-5 done

---
+++ Basics/02_dataTypes_lec_.js
@@ -0,0 +1,35 @@
+"use strict"; //This is typed to treat all JS code in the file according to newer version
+
+//alert (3+3) //gives error, since we are using node.js currently and not browser
+
+/*
+
+Types of data-types
+
+I- Primitive Data Type
+
+1. number (2^53)
+2. bigint 
+3. string
+4. boolean (true or false)
+5. null (standalone type)
+6. undefined 
+7. symbol (unique)
+
+II- NOn-Primitive Data types
+
+1. object
+
+*/
+
+
+
+//To get the type of any data- use type of
+
+let x = 5
+console.log(typeof x)
+console.log(typeof "Pranaw")
+
+console.log(typeof null) //gives object as output; a drawback in the JS language
+console.log(typeof undefined) //gives undefined as output; undefined is a type of itself
+
