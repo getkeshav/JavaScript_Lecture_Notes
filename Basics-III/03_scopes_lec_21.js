cfef41ab1e12596e4035d558497a6aab1f91076c
Pranaw-NIT-JSR <pramadhu.kumar@gmail.com>
2025-01-22 18:03:51 +0530

    lec-21 done

---
+++ Basics-III/03_scopes_lec_21.js
@@ -0,0 +1,38 @@
+//! Anything written between {} (curly braces) are block scope and outside that is global scope
+
+//! 'var' keyword is global and function scoped
+
+//! 'let' and 'const' keywords are block scoped and obviously function scoped
+{
+
+
+if (true) {
+    let a = 5
+    const b = 6
+    var c = 7
+    d = 8
+}
+
+console.log(a); //? gives error (since a can't be accessedd outside the scope)
+
+console.log(b); //? gives error (same reason)
+
+console.log(c); //? give 7 as the output, since c is accessible outside the scope
+
+console.log(d); //? 8 (same reason)
+
+}
+//Note: Global scope is differnet in node environment (here) and the browser windows (console)
+{
+
+    let a = 300
+
+    if (true) {
+        let a = 50
+        console.log(a); //since "a" already exist inside the block, it accesses that value of "a"
+        //Note: Agar block ke andar wala "a" nahi hota, tab bahar wala access ho jaata. Sicne bahar wala global scoped
+    }
+
+    console.log(a); // gives 300, since if block ke andar wala a is not accessible outside its scope (since declared usign let keyword)
+
+}
