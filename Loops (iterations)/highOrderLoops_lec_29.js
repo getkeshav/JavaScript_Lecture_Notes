e88122490519977c8583d636bed6c7009762f0fc
Pranaw-NIT-JSR <pramadhu.kumar@gmail.com>
2025-01-23 20:14:25 +0530

    lec-29 half done

---
+++ Loops (iterations)/highOrderLoops_lec_29.js
@@ -0,0 +1,26 @@
+{
+
+    //* for of
+
+
+    //* Using for of on array
+
+    const arr = ["Pranaw", 18, true, "Kumar", 2006]
+
+    //! Goes through each element of the array
+    for (const val of arr) {
+        console.log(val);
+    }
+
+    //* Using for of on string
+
+    const greetings = "Hello World!"
+
+    //! Goes through all individual characters in the string
+    for (const letter of greetings) {
+        console.log(letter);
+    }
+
+
+
+}
