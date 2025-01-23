7bde7d03ef88f3a712d3a5824b35178c2bdf15ca
Pranaw-NIT-JSR <pramadhu.kumar@gmail.com>
2025-01-23 10:48:11 +0530

    lec-26 done

---
+++ Control Flow_lec_26/if-else.js
@@ -0,0 +1,80 @@
+{
+  //only executed when condition is true
+  if (condition) {
+  }
+
+  //else-ifs are optional
+  else if (condition2) {
+  }
+
+  //else-ifs are optional
+  else if (condition3) {
+  }
+
+  //... (multiple else-if statements)
+
+  //else is optional
+  else {
+  }
+
+  //* Condition operator
+
+  // < , > , <=, >=, ==, ===, !=, !==
+
+  console.log(2 == "2"); //true (since value is same and type isn't compared here)
+
+  console.log(3 === "3"); //false (since type is not same)
+
+  console.log(2 != "2"); //false (since their value are considered equal)
+
+  console.log(3 !== "3"); //true (since their type are indeed not equal, so they are ultimately not equal)
+}
+
+{
+  //*Short-hand notation (implicit scope)
+  //! VVVV.Imp (; is necessary in this short-notation, to tell that our scope has ended)
+
+  const balance = 100;
+
+  if (balance > 50) console.log("I am inside if-block"); //Note: Here ; is necessary!!!! (to tell that our scope has ended!!!)
+  //! Here we didn't used curly braces, since only a single line code. Here, scope is implicitely defined
+
+  console.log("I am outside if-block");
+
+
+
+  if (balance > 50) console.log("Inside if"), console.log("still inside"); //! this is also acceptable. but not reccomaned at all!!
+
+  console.log("Now outside");
+}
+
+{
+  //* var
+
+  if (2 > 1) {
+    var power = "fly"; //Note: Since var is used, it's scope here is completely global!
+    console.log(`Power insinde if-block: ${power}`);
+  }
+
+  console.log(`Power outside if-block: ${power}`); //power is accessible outside if-block, becuase var keyword was used to define the power variable.
+  //! If 'let' or 'const' keyword had been used, then it would have thrown error...
+}
+
+{
+  //* Using && and ||
+
+  const userLoggedIn = true;
+  const debitCard = true;
+  const loggedInFromGoogle = false;
+  const loggedInFromEmail = true;
+
+  //executed when all the conditions are true simultaneously
+  if (userLoggedIn && debitCard && 2 == 3) {
+    console.log("Allow to buy course");
+  }
+
+  //exceuted immediately when a conditon becomes true (from left to right)
+  if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+  }
+}
---
+++ Control Flow_lec_26/truthy_falsy.js
@@ -0,0 +1,87 @@
+{
+  //* Truth and falsy values
+
+  const userEmail = h.mail.com;
+
+  if (userEmail) {
+    console.log("Got user email");
+  } else {
+    console.log("Don't have user email");
+  }
+
+  //* falsy values
+
+  // false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+  //* truthy values
+
+  // "0" (here, it is string), 'false' (here, it is string), " " (at least a space), [] (empty array), {} (empty object), function(){} (empty function), everything else left
+
+  // if (userEmail.length === 0) {
+  //     console.log("Array is empty");
+  // }
+}
+
+{
+
+    //* Check if array is empty
+
+    let arr = []
+
+    if (arr.length === 0) {
+        console.log("Array is empty!");
+    }
+
+    else {
+        console.log("Array is not empty!");
+    }
+
+    //* Check if object is empty
+
+    const obj = {}
+
+    //Note: Object.keys(obj_name) returns an array. Therfore, we used .length property to check that array's length just like in above to check whether the array is empty or not, which ultimately tells us wheter the object is empty or not
+
+    if (Object.keys(obj).length === 0) {
+        console.log("Object is empty!");
+    }
+
+    else {
+        console.log("Object is not empty!");
+    }
+
+}
+
+{
+    //* Interesting comparision
+
+    console.log(false == 0); //! true (since both false and 0 is considered falsy, therfore they are equal)
+
+    console.table(false == "");//! true
+
+    console.log(0 == "");//! true
+}
+
+{
+  //* Nullish Coalescing Operator (??) - used to check safety of null and undefined
+
+  //? If a null or undefined value is encountered, then the other value is assigned, else the first
+
+  console.log(5 ?? 10); //5
+  console.log(null ?? 8); //8 (since first is null)
+  console.log(6 ?? null); //6
+  console.log(undefined ?? 66); //66
+  console.log(7 ?? undefined); //7
+  console.log(12 ?? null ?? 9); //12
+  console.log(null ?? 86 ?? 25); //86
+}
+
+{
+  //* Ternary Operator
+
+  // condition ? true_statment : false_statement
+
+  const iceTeaPrice = 100;
+  iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+  
+}
