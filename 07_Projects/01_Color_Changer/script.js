1de1b7d66e1bbf623b81ad86d95f35572e6bab02
Pranaw-NIT-JSR <pramadhu.kumar@gmail.com>
2025-01-30 00:39:02 +0530

    project - 1 done

---
+++ 07_Projects/01_Color_Changer/script.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll(".button"); //gives a NodeList of all having class as "button"
+const myBody = document.querySelector("body"); //gives us the body of the document
+
+//here we assigned an event listener to each element of the NodeList to change the color to the element's id when clicked
+
+buttons.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    myBody.style.backgroundColor = e.target.id;
+  });
+});
