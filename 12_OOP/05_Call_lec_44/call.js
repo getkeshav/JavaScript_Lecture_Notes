cfff91c6d2d56c456480fcc083396364f710fa83
prana-w <pramadhu.kumar@gmail.com>
2025-02-06 12:21:25 +0530

    lec-44 done

---
+++ 12_OOP/05_Call_lec_44/call.js
@@ -0,0 +1,37 @@
+{
+
+
+    function setUsername (userName) {
+        this.userName = userName
+        console.log("Inside setUsername");
+    }
+
+    function createUser (userName, age, isLoggedIn) {
+
+        setUsername (userName) //we are outsourcing our setting userName step
+
+        this.age = age
+        this.isLoggedIn = isLoggedIn
+    }
+
+    const userOne = new createUser ("Pranaw Kumar", 18, true)
+
+    console.log(userOne); //! We see that even though we went inside "setUsername" function, we still can't set our userName. Why though?? See call.excalidraw last
+
+      //* "call" keyword
+
+    //? "call" keyword tranfers current execution-context to other function
+
+
+    function createUser2 (userName, age, isLoggedIn) {
+
+        setUsername.call (this, userName) //! We need to also pass "this" to tell the execution context we want to transfer
+        
+        this.age = age
+        this.isLoggedIn = isLoggedIn
+    }
+
+    const userTwo = new createUser2 ("xyz", 20, false)
+
+    console.log(userTwo);
+}
