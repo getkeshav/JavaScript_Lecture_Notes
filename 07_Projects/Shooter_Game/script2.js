ef9428611fd36bbfac91ed788dbcc33595b717f3
prana-w <pramadhu.kumar@gmail.com>
2025-03-13 23:12:12 +0530

    Add basic shoot logic (in +x direc)

--- 07_Projects/Shooter_Game/script2.js
+++ 07_Projects/Shooter_Game/script2.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("gameArea")
+const canvas = document.getElementById("gameArea");
 const bgctx = canvas.getContext("2d");
 const player1_score_element = document.getElementById("player1_score");
 const player1_speed_element = document.getElementById("player1_speed");
@@ -6,6 +6,8 @@ const player1_speed_element = document.getElementById("player1_speed");
 const player2_score_element = document.getElementById("player2_score");
 const player2_speed_element = document.getElementById("player2_speed");
 
+let movement_gun;
+let p, q;
 
 const ctx1 = document.getElementById("player1").getContext("2d");
 const ctx2 = document.getElementById("player2").getContext("2d");
@@ -14,14 +16,15 @@ const canvasColor = "grey";
 const player1_color = "red";
 const player2_color = "blue";
 const foodColor = "#ff3131";
-let time_period = 200; //time to move a unit for the snake(more the value, lesser the speed)
+let time_period1 = 200; //time to move a unit for the snake(more the value, lesser the speed)
+let time_period2 = 200;
 const playgroundSize = 500; //size of the playground
 const w = 20; //width of a unit of snake
 let score1 = 0; //keeps tracks of the score of the player1
 let score2 = 0;
 
-let speed1 = (w / time_period) * 1000;
-let speed2 = (w / time_period) * 1000;
+let speed1 = (w / time_period1) * 1000;
+let speed2 = (w / time_period2) * 1000;
 let movement1; //tracks current movement of player 1
 let movement2; //tracks current movement of player 2
 
@@ -57,12 +60,12 @@ function startNewGame(d = playgroundSize) {
   bgctx.fill();
 }
 function move_xP(t) {
-  if (t == 1 && p1X+w <= playgroundSize-w) {
-    p1X += w;  // Update position first
-    ctx1.clearRect(p1X - w, p1Y, w, w); 
+  if (t == 1 && p1X + w <= playgroundSize - w) {
+    p1X += w; // Update position first
+    ctx1.clearRect(p1X - w, p1Y, w, w);
     ctx1.fillStyle = player1_color;
     ctx1.fillRect(p1X, p1Y, w, w);
-  } else if (t == 2 && p2X+w <= playgroundSize-w) {
+  } else if (t == 2 && p2X + w <= playgroundSize - w) {
     p2X += w;
     ctx2.clearRect(p2X - w, p2Y, w, w);
     ctx2.fillStyle = player2_color;
@@ -71,12 +74,12 @@ function move_xP(t) {
 }
 
 function move_xN(t) {
-  if (t == 1 && p1X-w >= 0) {
+  if (t == 1 && p1X - w >= 0) {
     p1X -= w;
     ctx1.clearRect(p1X + w, p1Y, w, w);
     ctx1.fillStyle = player1_color;
     ctx1.fillRect(p1X, p1Y, w, w);
-  } else if (t == 2 && p2X-w >= 0) {
+  } else if (t == 2 && p2X - w >= 0) {
     p2X -= w;
     ctx2.clearRect(p2X + w, p2Y, w, w);
     ctx2.fillStyle = player2_color;
@@ -85,12 +88,12 @@ function move_xN(t) {
 }
 
 function move_yP(t) {
-  if (t == 1 && p1Y-w >= 0) {
+  if (t == 1 && p1Y - w >= 0) {
     p1Y -= w;
     ctx1.clearRect(p1X, p1Y + w, w, w);
     ctx1.fillStyle = player1_color;
     ctx1.fillRect(p1X, p1Y, w, w);
-  } else if (t == 2 && p2Y-w >= 0) {
+  } else if (t == 2 && p2Y - w >= 0) {
     p2Y -= w;
     ctx2.clearRect(p2X, p2Y + w, w, w);
     ctx2.fillStyle = player2_color;
@@ -99,19 +102,18 @@ function move_yP(t) {
 }
 
 function move_yN(t) {
-  if (t == 1 && p1Y+w <= playgroundSize-w) {
+  if (t == 1 && p1Y + w <= playgroundSize - w) {
     p1Y += w;
     ctx1.clearRect(p1X, p1Y - w, w, w);
     ctx1.fillStyle = player1_color;
     ctx1.fillRect(p1X, p1Y, w, w);
-  } else if (t == 2 && p2Y+w <= playgroundSize-w) {
+  } else if (t == 2 && p2Y + w <= playgroundSize - w) {
     p2Y += w;
     ctx2.clearRect(p2X, p2Y - w, w, w);
     ctx2.fillStyle = player2_color;
     ctx2.fillRect(p2X, p2Y, w, w);
   }
 }
-
 
 //Adding controls to the game
 window.addEventListener("keydown", (e) => {
@@ -120,52 +122,60 @@ window.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "ArrowLeft":
       clearInterval(movement2);
-      movement2 = setInterval(move_xN, time_period, 2);
+      movement2 = setInterval(move_xN, time_period2, 2);
       break;
 
     case "ArrowRight":
       clearInterval(movement2);
-      movement2 = setInterval(move_xP, time_period, 2);
+      movement2 = setInterval(move_xP, time_period2, 2);
       break;
 
     case "ArrowUp":
       clearInterval(movement2);
-      movement2 = setInterval(move_yP, time_period, 2);
+      movement2 = setInterval(move_yP, time_period2, 2);
       break;
 
     case "ArrowDown":
       clearInterval(movement2);
-      movement2 = setInterval(move_yN, time_period, 2);
+      movement2 = setInterval(move_yN, time_period2, 2);
       break;
 
     case "A":
     case "a":
       clearInterval(movement1);
-      movement1 = setInterval(move_xN, time_period, 1);
+      movement1 = setInterval(move_xN, time_period1, 1);
       break;
 
     case "D":
     case "d":
       clearInterval(movement1);
-      movement1 = setInterval(move_xP, time_period, 1);
+      movement1 = setInterval(move_xP, time_period1, 1);
       break;
 
     case "W":
     case "w":
       clearInterval(movement1);
-      movement1 = setInterval(move_yP, time_period, 1);
+      movement1 = setInterval(move_yP, time_period1, 1);
       break;
 
     case "S":
     case "s":
       clearInterval(movement1);
-      movement1 = setInterval(move_yN, time_period, 1);
+      movement1 = setInterval(move_yN, time_period1, 1);
       break;
 
     case " ":
       clearInterval(movement1);
       clearInterval(movement2);
       break;
+
+    case "p":
+      clearInterval(movement_gun)
+
+      ctx1.clearRect(p , q, w / 2.5, w / 2.5);
+    p = p1X + w;
+    q = p1Y + w / 2.5;
+      movement_gun = setInterval(shoot, time_period1 / 2)
   }
 });
 
@@ -204,3 +214,16 @@ window.addEventListener("keydown", (e) => {
 
 startNewGame();
 // scoreUp();
+
+
+function shoot() {
+  p += w / 2.5;
+  ctx1.clearRect(p - w / 2.5, q, w / 2.5, w / 2.5);
+  ctx1.fillStyle = "yellow";
+  ctx1.fillRect(p, q, w / 2.5, w / 2.5);
+
+  if (p >= playgroundSize) {
+    clearInterval(movement_gun);
+    console.log("Cleared!");
+  }
+}
