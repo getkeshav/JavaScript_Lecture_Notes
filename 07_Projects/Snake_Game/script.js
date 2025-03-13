5704d306a3594d446c5cd2689e884800b22d484b
prana-w <pramadhu.kumar@gmail.com>
2025-03-13 14:09:46 +0530

    Add basic implementatin of snake game

---
+++ 07_Projects/Snake_Game/script.js
@@ -0,0 +1,97 @@
+const canvas = document.getElementById("gameArea");
+const ctx = canvas.getContext("2d");
+const canvasColor = "#d599de";
+const snakeColor = "red";
+let time_period = 500; //time to move a unit for the snake(more the value, lesser the speed)
+const playgroundSize = 500; //size of the playground
+const w = 15; //width of a unit of snake
+
+//creates the playground (canvas and snake)
+function startNewGame(d = playgroundSize) {
+  if (!canvas.getContext) {
+    alert("Canvas not supported in your browser!");
+    return;
+  }
+  ctx.fillStyle = canvasColor;
+  ctx.beginPath(); //starts a new shape path
+  ctx.moveTo(0, 0); //goes to an initial point
+  ctx.lineTo(d, 0); //moves to another point for line
+  ctx.lineTo(d, d);
+  ctx.lineTo(0, d);
+  ctx.lineTo(0, 0);
+  ctx.fill();
+
+  ctx.save() //saves the inital state of the canvas
+
+  //add initial snake body
+  ctx.fillStyle = snakeColor;
+  ctx.fillRect(0, 0, w, w);
+
+  move_xP()
+}
+
+
+//moves the snake in positive x direction
+function move_xP() {
+
+
+
+    const xp = setInterval (() => {
+        ctx.fillStyle = canvasColor;
+        ctx.fillRect(0, 0, w, w);
+      
+        ctx.fillStyle = snakeColor;
+        ctx.translate(w, 0);
+        ctx.fillRect(0, 0, w, w);
+    }, time_period)
+  
+}
+
+// //moves the snake in positive x direction
+// function move_xN() {
+//     ctx.fillStyle = canvasColor;
+//     ctx.fillRect(0, 0, w, w);
+  
+//     ctx.fillStyle = snakeColor;
+//     ctx.translate(w, 0);
+//     ctx.fillRect(0, 0, w, w);
+//   }
+
+// //moves the snake in positive x direction
+// function move_yP() {
+//     ctx.fillStyle = canvasColor;
+//     ctx.fillRect(0, 0, w, w);
+  
+//     ctx.fillStyle = snakeColor;
+//     ctx.translate(w, 0);
+//     ctx.fillRect(0, 0, w, w);
+//   }
+
+  //moves the snake in positive x direction
+// function move_yN() {
+
+//     const yp = setInterval (() => {
+//         ctx.fillStyle = canvasColor;
+//         ctx.fillRect(0, 0, w, w);
+      
+//         ctx.fillStyle = snakeColor;
+//         ctx.translate(0, w);
+//         ctx.fillRect(0, 0, w, w);
+//     }, time_period)
+    
+//   }
+
+// function isDead (d = playgroundSize) {
+//     if 
+// }
+
+
+
+startNewGame();
+
+// window.addEventListener ("keydown", (e) => {
+//     if (e.key = "down") {
+//         clearInterval (movexP)
+        
+//     }
+// })
