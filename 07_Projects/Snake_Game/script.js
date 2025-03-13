82ac2eb89bd8f98d175203592fd7e638330e715b
prana-w <pramadhu.kumar@gmail.com>
2025-03-13 19:28:00 +0530

    Add CSS to snake game

--- 07_Projects/Snake_Game/script.js
+++ 07_Projects/Snake_Game/script.js
@@ -1,23 +1,22 @@
 const bgctx = document.getElementById("gameArea").getContext("2d");
-const player_score_element = document.getElementById("player_score")
-const player_speed_element = document.getElementById("player_speed")
+const player_score_element = document.getElementById("player_score");
+const player_speed_element = document.getElementById("player_speed");
 const canvas = document.getElementById("snake");
 const ctx = canvas.getContext("2d");
-const canvasColor = "#c5fcf4";
-const snakeColor = "red";
-let time_period = 200; //time to move a unit for the snake(more the value, lesser the speed)
+const canvasColor = "#1a472a";
+const snakeColor = "#4cff4c";
+const foodColor = "#ff3131";
+let time_period = 100; //time to move a unit for the snake(more the value, lesser the speed)
 const playgroundSize = 500; //size of the playground
 const w = 15; //width of a unit of snake
 let score = 0; //keeps tracks of the score of the player
-let speed = (w/time_period)*1000
-
-player_speed_element.innerText = Math.floor(speed)
-
+let speed = (w / time_period) * 1000;
 
 //snake coordinates
+let x = w / 2;
+let y = w / 2;
 
-let x = w/2
-let y = w/2
+player_speed_element.innerText = Math.floor(speed);
 
 let movement; //tracks current movement
 
@@ -35,11 +34,10 @@ function startNewGame(d = playgroundSize) {
   bgctx.lineTo(0, d);
   bgctx.lineTo(0, 0);
   bgctx.fill();
-
 }
-  //add initial snake body 
-  ctx.fillStyle = snakeColor;
-  ctx.fillRect(0, 0, w, w);
+//add initial snake body
+ctx.fillStyle = snakeColor;
+ctx.fillRect(0, 0, w, w);
 //moves the snake in positive x direction
 function move_xP() {
   ctx.fillStyle = canvasColor;
@@ -49,8 +47,8 @@ function move_xP() {
   ctx.translate(w, 0);
   ctx.fillRect(0, 0, w, w);
 
-  x += w
-  y += 0
+  x += w;
+  y += 0;
 }
 //moves the snake in negative x direction
 function move_xN() {
@@ -61,8 +59,8 @@ function move_xN() {
   ctx.translate(-w, 0);
   ctx.fillRect(0, 0, w, w);
 
-  x -= w
-  y += 0
+  x -= w;
+  y += 0;
 }
 //moves the snake in positive y direction
 function move_yP() {
@@ -73,8 +71,8 @@ function move_yP() {
   ctx.translate(0, -w);
   ctx.fillRect(0, 0, w, w);
 
-  x += 0
-  y -= w
+  x += 0;
+  y -= w;
 }
 //   moves the snake in negative y direction
 function move_yN() {
@@ -85,78 +83,74 @@ function move_yN() {
   ctx.translate(0, w);
   ctx.fillRect(0, 0, w, w);
 
-  x += 0
-  y += w
+  x += 0;
+  y += w;
 }
 //Adding controls to the game
 window.addEventListener("keydown", (e) => {
-    e.preventDefault();
-  
-    switch (e.key) {
-      case "ArrowLeft":
-      case "A":
-      case "a":
-        clearInterval(movement);
-        movement = setInterval(move_xN, time_period);
-        break;
-  
-      case "ArrowRight":
-      case "D":
-      case "d":
-        clearInterval(movement);
-        movement = setInterval(move_xP, time_period);
-        break;
-  
-      case "ArrowUp":
-      case "W":
-      case "w":
-        clearInterval(movement);
-        movement = setInterval(move_yP, time_period);
-        break;
-  
-      case "ArrowDown":
-      case "S":
-      case "s":
-        clearInterval(movement);
-        movement = setInterval(move_yN, time_period);
-        break;
-  
-      case " ":
-        clearInterval(movement);
-        break;
-    }
-  });
+  e.preventDefault();
 
-  //TODO: this increses the score of the player
-  function scoreUp(d = playgroundSize) {
+  switch (e.key) {
+    case "ArrowLeft":
+    case "A":
+    case "a":
+      clearInterval(movement);
+      movement = setInterval(move_xN, time_period);
+      break;
 
-    let a = Math.floor(Math.random()*d)
-    let b = Math.floor(Math.random()*d)
+    case "ArrowRight":
+    case "D":
+    case "d":
+      clearInterval(movement);
+      movement = setInterval(move_xP, time_period);
+      break;
 
-    //adds food
-    bgctx.fillStyle = "blue";
-  bgctx.fillRect(a, b, w/4, w/4);
+    case "ArrowUp":
+    case "W":
+    case "w":
+      clearInterval(movement);
+      movement = setInterval(move_yP, time_period);
+      break;
 
-  const collision = setInterval (() => {
-    if (Math.abs(x-a) <= w && Math.abs(y-b) <= w) {
-        console.log("Collision!");
-        score += 10;
-        player_score_element.innerText = score
-        time_period -= 5
-        speed = (w/time_period)*1000
-        console.log(speed);
-        player_speed_element.innerText = Math.floor(speed)
-        clearInterval(collision)
-        startNewGame()
-        scoreUp()
-    }
-}, 10)
+    case "ArrowDown":
+    case "S":
+    case "s":
+      clearInterval(movement);
+      movement = setInterval(move_yN, time_period);
+      break;
 
+    case " ":
+      clearInterval(movement);
+      break;
   }
-  
+});
+
+function scoreUp(d = playgroundSize) {
+  let a = Math.floor(Math.random() * d);
+  let b = Math.floor(Math.random() * d);
+
+  console.log("Food:", a, b);
+
+  //adds food
+  bgctx.fillStyle = foodColor;
+  bgctx.fillRect(a, b, w / 3, w / 3);
+
+  //! Check if conditiopn for collision
+  const collision = setInterval(() => {
+    if (Math.abs(x - a) <= w && Math.abs(y - b) <= w) {
+      console.log("Collision!");
+      score += 10;
+      player_score_element.innerText = score;
+      time_period -= 5;
+      speed = (w / time_period) * 1000;
+      console.log(speed);
+      player_speed_element.innerText = Math.floor(speed);
+      clearInterval(collision);
+      startNewGame();
+      setTimeout (scoreUp, 0)
+    }
+  }, 10);
+}
+
 startNewGame();
-scoreUp()
-
-
-
-
+scoreUp();
