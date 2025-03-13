13da9f466f1ebcc4531ec7396af2fa94181a65a2
prana-w <pramadhu.kumar@gmail.com>
2025-03-13 17:16:28 +0530

    Add multi-layer canvas

--- 07_Projects/Snake_Game/script.js
+++ 07_Projects/Snake_Game/script.js
@@ -1,10 +1,12 @@
-const canvas = document.getElementById("gameArea");
+const bgctx = document.getElementById("gameArea").getContext("2d");
+const canvas = document.getElementById("snake");
 const ctx = canvas.getContext("2d");
-const canvasColor = "#d599de";
+const canvasColor = "#c5fcf4";
 const snakeColor = "red";
 let time_period = 300; //time to move a unit for the snake(more the value, lesser the speed)
 const playgroundSize = 500; //size of the playground
 const w = 15; //width of a unit of snake
+let score = 0; //keeps tracks of the score of the player
 
 let movement; //tracks current movement
 
@@ -14,20 +16,24 @@ function startNewGame(d = playgroundSize) {
     alert("Canvas not supported in your browser!");
     return;
   }
-  ctx.fillStyle = canvasColor;
-  ctx.beginPath(); //starts a new shape path
-  ctx.moveTo(0, 0); //goes to an initial point
-  ctx.lineTo(d, 0); //moves to another point for line
-  ctx.lineTo(d, d);
-  ctx.lineTo(0, d);
-  ctx.lineTo(0, 0);
-  ctx.fill();
+  bgctx.fillStyle = canvasColor;
+  bgctx.beginPath(); //starts a new shape path
+  bgctx.moveTo(0, 0); //goes to an initial point
+  bgctx.lineTo(d, 0); //moves to another point for line
+  bgctx.lineTo(d, d);
+  bgctx.lineTo(0, d);
+  bgctx.lineTo(0, 0);
+  bgctx.fill();
 
-  ctx.save(); //saves the inital state of the canvas
+  bgctx.save(); //saves the inital state of the canvas
 
-  //add initial snake body
+  //add initial snake body 
   ctx.fillStyle = snakeColor;
   ctx.fillRect(0, 0, w, w);
+
+//   //add initial snake body (player 2)
+//   ctx.fillStyle = snakeColor;
+//   ctx.fillRect(0, 0, w, w);
 }
 
 //moves the snake in positive x direction
@@ -39,8 +45,7 @@ function move_xP() {
   ctx.translate(w, 0);
   ctx.fillRect(0, 0, w, w);
 }
-
-//moves the snake in positive x direction
+//moves the snake in negative x direction
 function move_xN() {
   ctx.fillStyle = canvasColor;
   ctx.fillRect(0, 0, w, w);
@@ -49,8 +54,7 @@ function move_xN() {
   ctx.translate(-w, 0);
   ctx.fillRect(0, 0, w, w);
 }
-
-//moves the snake in positive x direction
+//moves the snake in positive y direction
 function move_yP() {
   ctx.fillStyle = canvasColor;
   ctx.fillRect(0, 0, w, w);
@@ -59,8 +63,7 @@ function move_yP() {
   ctx.translate(0, -w);
   ctx.fillRect(0, 0, w, w);
 }
-
-//   moves the snake in positive x direction
+//   moves the snake in negative y direction
 function move_yN() {
   ctx.fillStyle = canvasColor;
   ctx.fillRect(0, 0, w, w);
@@ -69,44 +72,53 @@ function move_yN() {
   ctx.translate(0, w);
   ctx.fillRect(0, 0, w, w);
 }
-
-startNewGame();
-
 //Adding controls to the game
 window.addEventListener("keydown", (e) => {
-  e.preventDefault();
+    e.preventDefault();
+  
+    switch (e.key) {
+      case "ArrowLeft":
+      case "A":
+      case "a":
+        clearInterval(movement);
+        movement = setInterval(move_xN, time_period);
+        break;
+  
+      case "ArrowRight":
+      case "D":
+      case "d":
+        clearInterval(movement);
+        movement = setInterval(move_xP, time_period);
+        break;
+  
+      case "ArrowUp":
+      case "W":
+      case "w":
+        clearInterval(movement);
+        movement = setInterval(move_yP, time_period);
+        break;
+  
+      case "ArrowDown":
+      case "S":
+      case "s":
+        clearInterval(movement);
+        movement = setInterval(move_yN, time_period);
+        break;
+  
+      case " ":
+        clearInterval(movement);
+        break;
+    }
+  });
 
-  switch (e.key) {
-    case "ArrowLeft":
-    case "A":
-    case "a":
-      clearInterval(movement);
-      movement = setInterval(move_xN, time_period);
-      break;
+  //this increses the score of the player
+  function scoreUp() {
 
-    case "ArrowRight":
-    case "D":
-    case "d":
-      clearInterval(movement);
-      movement = setInterval(move_xP, time_period);
-      break;
+    ctx.save()
+    ctx.restore()
 
-    case "ArrowUp":
-    case "W":
-    case "w":
-      clearInterval(movement);
-      movement = setInterval(move_yP, time_period);
-      break;
 
-    case "ArrowDown":
-    case "S":
-    case "s":
-      clearInterval(movement);
-      movement = setInterval(move_yN, time_period);
-      break;
-
-    case " ":
-      clearInterval(movement);
-      break;
   }
-});
+  
+startNewGame();
+
