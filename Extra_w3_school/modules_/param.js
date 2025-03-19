873d9f8c9543b5c28eabc4551af89d8b26cc5b7f
prana-w <pramadhu.kumar@gmail.com>
2025-03-19 18:16:24 +0530

    learnt new concepts from articles

---
+++ Extra_w3_school/modules_/param.js
@@ -0,0 +1,18 @@
+// export const name = "Pranaw Kumar"  
+// export const age = 18
+// export const isOnline = true
+
+
+const name = "Pranaw Kumar"  
+const age = 18
+const isOnline = true
+
+
+function example() {
+    console.log("hello World!");
+}
+
+
+// export {name, age, isOnline, example}
+
+export default example
---
+++ Extra_w3_school/modules_/use_para.js
@@ -0,0 +1,8 @@
+// import {name, age, isOnline, example} from "./param.js";
+
+import example from "./param.js";
+
+// console.log(`My name is ${name} and my age is ${age}`);
+
+example()
+
