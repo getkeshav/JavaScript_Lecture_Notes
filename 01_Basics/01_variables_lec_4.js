8a3c127bff1f2c231ca62c8859aec4fac470f254
getkeshav <84590659+getkeshav@users.noreply.github.com>
2025-05-06 22:58:16 +0530

    Add information about redeclaration of var, let and const

--- 01_Basics/01_variables_lec_4.js
+++ 01_Basics/01_variables_lec_4.js
@@ -5,12 +5,15 @@
 
 const accountId = 5
 //accountId = 2 //! Not allowed (since const can't be redefinded!!!)
+// const accountId = 6 //! Not allowed (since const can't be redeclared!!!)
 
 let accountCity = "Delhi"
 accountCity = "Jharkhand" //Allowed (since let allows redefinition)
+// let accountCity = 'Delhi' //! Not allowed (since let can't be redeclared!!!)
 
 var accountName = "Pranaw" //var keyword not recommended!!!! 
 accountName = "ABC" //Allowed (since var allows redifintion)
+var accountName = "Poorav" //Allowed (since var allows redeclaration)
 
 accountPass = 5 //declared without any keyword (not at all recommneded!!!)
 
